1f1ba9f1efdecdd1e1c5ca9257dade3faf8a0282
dan <dan.debiazi@gmail.com>
2022-09-01 21:06:33 -0400

    General refactoring

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -25,10 +25,11 @@ export class HomeComponent {
     this.typedWords = [];
     this.checkedWords = [];
 
+    //The data will fill the cmbLanguage field.
     this.serviceLanguage.getLanguages().subscribe(data => {
       this.languages = data;
       if (data.length <= 0)
-        this.serviceNotification.showError("Error to load languages. Try again later.", "Error");
+        this.serviceNotification.showError("There is no language data to show. Contact the administrator.", "Error");
 
       console.debug("Languages", this.languages);
     }, error => {
@@ -52,13 +53,16 @@ export class HomeComponent {
     this.clearForm();
   }
 
-  checkWords(form: any) {
+  btncheckWordsClick(form: any) {
     try {
       let text = this.removeExtraSpace(form.value.text)
+      //Get language from combobox cmblanguages
       this.selectedLanguage = form.value.cmblanguages ?? "en";
       this.clearForm();
       if (text != "") {
+        //Split all word in an array
         this.typedWords = text.split(' ');
+        //Remove duplicated words
         this.typedWords = [...new Set(this.typedWords)];
         this.typedWords.forEach(word => {
           this.serviceWord.getWordByWordAndLanguage(word, this.selectedLanguage).subscribe(data => {
@@ -81,19 +85,25 @@ export class HomeComponent {
   }
 
   saveWords() {
+    //Validate if there is words to be checked
     if (this.checkedWords.length <= 0) {
       this.serviceNotification.showWarning("Choose the words you know before save it.", "Warning");
       return;
     }
 
+    //For each word of checkedWords array do:
     this.checkedWords.forEach(word => {
+      //Get word and language from DB
       this.serviceWord.getWordByWordAndLanguage(word, this.selectedLanguage).subscribe(data => {
+        //If there is word and language from the DB equal to the word and language the user is trying to save, skip it.
         let isWordDuplicated = data != null;
         if (isWordDuplicated)
           return;
 
+        //For now, ignore the userId
         let userId = localStorage.getItem("userId") ?? 0;
         console.debug("Chosen Language", this.selectedLanguage);
+        //Saving the word and language (and userId)
         this.serviceWord.saveWord({ languageId: this.selectedLanguage, userId: userId, wordName: word }).subscribe(() => {
           this.serviceNotification.showSuccess("Word(s) saved.", "Success");
           console.debug("Saved Words", this.checkedWords);
@@ -108,14 +118,19 @@ export class HomeComponent {
   }
 
   isDefaultChecked(item: string) {
+    //Check if the word exist in the DB and mark it as checked(green)
     return this.wordsFromDB.includes(item.trim().toLowerCase())
   }
 
+  //This event is triggered when the user check/uncheck a word
   wordChkSelect(event: any) {
+    //check if the word is checked already, if it is already in the checkedWords variable(avoiding duplicate words) and if
+    //the word is already in the DB
     if (event.target.checked && !this.checkedWords.includes(event.target.value) && !this.wordsFromDB.includes(event.target.value)) {
       this.checkedWords.push(event.target.value);
       console.debug("Word Included", this.checkedWords);
     } else if (!event.target.checked) {
+      //remove the unchecked word from checkedWords variable
       var index = this.checkedWords.indexOf(event.target.value);
       if (index !== -1) {
         this.checkedWords.splice(index, 1);
