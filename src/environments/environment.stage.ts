2b1480792068145d5c3254adbcc9cb825d9a67a8
dan <dan.debiazi@gmail.com>
2022-10-06 12:40:41 -0400

    Added stage environment config file

---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_URL: "https://thewordsthatiknowapi-git-stage-pankwood.vercel.app/",
+  API_LOGIN: "thewordthatiknow",
+  API_PASSWORD: "thewordthatiknow200",
+};
