d6b5a30e661e26fb3c1b434a953516ac0e91fa90
dan <dan.debiazi@gmail.com>
2022-10-06 13:44:51 -0400

    Removed Git version and Vercel Environment

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -3,6 +3,4 @@ export const environment = {
   API_URL: "https://thewordsthatiknowapi-git-stage-pankwood.vercel.app/",
   API_LOGIN: "thewordthatiknow",
   API_PASSWORD: "thewordthatiknow200",
-  ANGULAR_APP_VERCEL_GIT_COMMIT_SHA: "",
-  ANGULAR_APP_VERCEL_ENV: "",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,6 @@ export const environment = {
   API_URL: "http://localhost:5000/",
   API_LOGIN: "thewordthatiknow",
   API_PASSWORD: "thewordthatiknow200",
-  ANGULAR_APP_VERCEL_GIT_COMMIT_SHA: "",
-  ANGULAR_APP_VERCEL_ENV: "",
 };
 
 /*
