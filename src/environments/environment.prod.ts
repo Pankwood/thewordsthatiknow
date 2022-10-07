4aa1298a1c53c46fc7464e8aa877c0e57f89b20c
dan <dan.debiazi@gmail.com>
2022-10-07 10:28:19 -0400

    Removed environments files from repository

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: "https://thewordsthatiknowapi.vercel.app/",
-  API_LOGIN: "thewordthatiknow",
-  API_PASSWORD: "thewordthatiknow200",
-  NAME: "Prod",
-};
--- src/environments/environment.stage.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  API_URL: "https://thewordsthatiknowapi-git-stage-pankwood.vercel.app/",
-  API_LOGIN: "thewordthatiknow",
-  API_PASSWORD: "thewordthatiknow200",
-  NAME: "Stage",
-};
