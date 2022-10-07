4c268aef4327b8e60335ef9a404f8ce5fbebb929
dan <dan.debiazi@gmail.com>
2022-10-07 10:51:14 -0400

    Fixed stage environment. Attempt 6

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  API_URL: "https://thewordsthatiknowapi.vercel.app/",
+  API_LOGIN: "thewordthatiknow",
+  API_PASSWORD: "thewordthatiknow200",
+  NAME: "Prod",
+};
