8ccbe96dc1bc1e40742a447061e9a1eb3dc11ba0
dan <dan.debiazi@gmail.com>
2022-10-07 09:22:06 -0400

    Fixed stage environment

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   API_URL: "https://thewordsthatiknowapi-git-stage-pankwood.vercel.app/",
   API_LOGIN: "thewordthatiknow",
   API_PASSWORD: "thewordthatiknow200",
