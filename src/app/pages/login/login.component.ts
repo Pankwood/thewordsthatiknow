e13b390d9c491537c3db77600ab7659a5de1287e
dan <dan.debiazi@gmail.com>
2022-10-25 10:55:47 -0400

    Added login validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { ModalService } from 'src/app/services/modal.service';
 import { AuthService } from './../../services/auth.service';
 import { Router } from "@angular/router";
 import { NotificationService } from 'src/app/services/notification.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +11,40 @@ import { NotificationService } from 'src/app/services/notification.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  invalidLogin: boolean = false;
+  correctCredentials: boolean = true;
   modalId: string;
   constructor(private modalService: ModalService, private authService: AuthService, private router: Router, private serviceNotification: NotificationService) {
     this.modalId = modalService.modalLoginID;
   }
 
-  login(credentials: any) {
-    this.authService.login(credentials)
-      .subscribe(result => {
-        console.debug(result);
-        if (result)
-          this.router.navigate(['/']);
-        else
-          this.invalidLogin = true;
-      }, error => {
-        this.invalidLogin = true;
-        this.serviceNotification.showError("Error to login. Try again later.", "Error");
-        console.debug("Error login", error);
+  login(form: any) {
+    if (form.valid) {
+      const credentials = form.value;
+      this.authService.login(credentials)
+        .subscribe(result => {
+          if (result) {
+            //this.router.navigate(['/']);
+            this.closeLoginModal(this.modalId);
+            this.correctCredentials = true;
+          }
+          else {
+            this.correctCredentials = false;
+          }
+        }, error => {
+          if (error.status === 404 || error.status === 401) {
+            this.correctCredentials = false;
+          }
+          else
+            this.serviceNotification.showError("Error to login. Try again later.", "Error");
+          this.authService.logout();
+          console.debug("Error login", error);
+        });
+    }
+    else {
+      Object.keys(form.controls).forEach(key => {
+        form.controls[key].markAsDirty();
       });
-
-    this.closeLoginModal(this.modalId);
+    }
   }
 
   openLoginModal(modalId: string) {
