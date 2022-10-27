c2b43380a2e794dffad64b48ac7b68a784ac2ae4
NaiaOzorio <naia.ozorio@gmail.com>
2022-10-26 20:18:14 -0400

    Added tutorial validation

--- e2e-tests/cypress/integration/mainPage.spec.js
+++ e2e-tests/cypress/integration/mainPage.spec.js
@@ -5,7 +5,22 @@ before('Visit web page', function() {
 })  
 
 describe('Validade Header', function(){
-    it('Validade title, app name and logo', function() {
+    it('Title, app name and logo', function() {
+        cy.contains('The Words That I Know')
+        cy.get('#divLogo img').should('be.visible')
+    })
+})
+
+describe('Validate Tutorial section', function(){
+    it('Text, subtitle and video', function() {
+        cy.contains('How many words do you think you know?')
+        cy.contains("Let's figure this out right now!")
+        // falta o teste do video
+    })
+})
+
+describe('Validade Language selection', function(){
+    it('Title, app name and logo', function() {
         cy.contains('The Words That I Know')
         cy.get('#divLogo img').should('be.visible')
     })
